86c1b75aa92088c2c9db4dd419693e00f2df965d
TonyDznelai <132153082+TonyDznelai@users.noreply.github.com>
2023-06-02 21:21:30 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+});
+
+
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+    effect: "fade",
+
+  });
+
